df1110cf3466acfad78e0e0d1a0c07b62159c9aa
Danielalab <olabolacrayola@hotmail.com>
2020-03-10 11:35:24 -0500

    creating arrTaks observable

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
+  private arrTasksSource = new BehaviorSubject([]);
+  currentArrTasks = this.arrTasksSource.asObservable;
 
   constructor() { }
+
+  addTask(task) {
+    console.log(this.arrTasksSource);
+  }
+
 }
