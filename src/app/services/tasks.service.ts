540f01913d23d0e4c1efb4190d971d3463a528a7
Danielalab <olabolacrayola@hotmail.com>
2020-03-10 11:45:33 -0500

    adding addTask method

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -11,7 +11,18 @@ export class TasksService {
   constructor() { }
 
   addTask(task) {
-    console.log(this.arrTasksSource);
+    // creando el obj con la data de la tarea
+    const taskObj = {
+      text: task,
+      id: this.arrTasksSource.value.length + 1
+    };
+    // creando el arr actualizado con la nueva tarea
+    const newArrTasks = [
+      ...this.arrTasksSource.value,
+      taskObj
+    ]
+    // actualizando el valor del arr de tareas
+    this.arrTasksSource.next(newArrTasks);
   }
 
 }
