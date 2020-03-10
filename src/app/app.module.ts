bc569b174ac38698602ee6ba1cd6faf9a7422ae3
Danielalab <olabolacrayola@hotmail.com>
2020-03-10 10:44:40 -0500

    adding template form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PapaComponent } from './components/papa/papa.component';
-import { HijoComponent } from './components/hijo/hijo.component';
 import { FormAddTareaComponent } from './components/form-add-tarea/form-add-tarea.component';
 import { ListTasksComponent } from './components/list-tasks/list-tasks.component';
 import { TaskComponent } from './components/task/task.component';
@@ -12,8 +10,6 @@ import { TaskComponent } from './components/task/task.component';
 @NgModule({
   declarations: [
     AppComponent,
-    PapaComponent,
-    HijoComponent,
     FormAddTareaComponent,
     ListTasksComponent,
     TaskComponent
