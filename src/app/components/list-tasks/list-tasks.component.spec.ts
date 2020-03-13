f7225074323a37d196b94277725e5e6ee462f5f3
Danielalab <olabolacrayola@hotmail.com>
2020-03-13 12:39:17 -0500

    adding tests list-task component

--- src/app/components/list-tasks/list-tasks.component.spec.ts
+++ src/app/components/list-tasks/list-tasks.component.spec.ts
@@ -1,14 +1,37 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListTasksComponent } from './list-tasks.component';
+import { TasksService, Task } from 'src/app/services/tasks.service';
+import { of } from 'rxjs';
+import { Component } from '@angular/core';
+
+@Component({selector: 'app-task', template: '<li></li>'})
+class TaskStubComponent {}
 
 describe('ListTasksComponent', () => {
   let component: ListTasksComponent;
   let fixture: ComponentFixture<ListTasksComponent>;
+  let tasksService;
+  let tasksServiceStub: Partial<TasksService>;
+  let arrTasksTest: Task[];
+  let ulEl: HTMLElement;
 
   beforeEach(async(() => {
+    arrTasksTest = [
+      { id: '1', text: 'text 1', complete: false },
+      { id: '2', text: 'text 2', complete: false }
+    ];
+
+    tasksServiceStub = {
+      currentArrTasks: of(arrTasksTest),
+    };
+
     TestBed.configureTestingModule({
-      declarations: [ ListTasksComponent ]
+      declarations: [
+        ListTasksComponent,
+        TaskStubComponent
+      ],
+      providers: [ { provide: TasksService, useValue: tasksServiceStub } ]
     })
     .compileComponents();
   }));
@@ -16,10 +39,40 @@ describe('ListTasksComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ListTasksComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    // TasksService from the root injector
+    tasksService = TestBed.inject(TasksService);
+    ulEl = fixture.nativeElement;
+    // fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Deberia de mostrar una lista de 2 tareas', () => {
+    // inicialmente ul deberia de estar vacio
+    expect(ulEl.querySelectorAll('li').length).toBe(0);
+
+    // al ejecutar ngOnInit()
+    fixture.detectChanges();
+
+    // ulEl debe tener 2 nodos hijos
+    expect(ulEl.querySelectorAll('li').length).toBe(2);
+  });
+
+  it('Deberia de mostrar una lista de 3 tareas', () => {
+    const newArrTasks = [
+      { id: 1, text: 'text 1' },
+      { id: 2, text: 'text 2' },
+      { id: 3, text: 'text 3' }
+    ]
+    // cambiar el valor de retorno de currentArrTasks
+    tasksService.currentArrTasks = of(newArrTasks);
+
+    fixture.detectChanges();
+
+    // ulEl debe tener 3 nodos hijos
+    expect(ulEl.querySelectorAll('li').length).toBe(3);
+  });
+  
 });
