28cabda604cfb3b04a24b7f91d03e55dbd4225e8
Danielalab <olabolacrayola@hotmail.com>
2020-03-14 22:21:24 -0500

    adding only example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormAddTareaComponent } from './components/form-add-tarea/form-add-tarea.component';
-import { ListTasksComponent } from './components/list-tasks/list-tasks.component';
-import { TaskComponent } from './components/task/task.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { ButtonsComponent } from './components/buttons/buttons.component';
 import { ShowNameComponent } from './components/show-name/show-name.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormAddTareaComponent,
-    ListTasksComponent,
-    TaskComponent,
     ButtonsComponent,
     ShowNameComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
   ],
   providers: [
     
