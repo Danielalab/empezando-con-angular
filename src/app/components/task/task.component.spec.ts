6bbbec79ee9a1e8898d86acecd715faa8b03188e
Danielalab <olabolacrayola@hotmail.com>
2020-03-20 12:28:45 -0500

    adding test Task component

--- src/app/components/task/task.component.spec.ts
+++ src/app/components/task/task.component.spec.ts
@@ -1,10 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TaskComponent } from './task.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('TaskComponent', () => {
   let component: TaskComponent;
   let fixture: ComponentFixture<TaskComponent>;
+  let liDe: DebugElement;
+  let liEl: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +20,21 @@ describe('TaskComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TaskComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    liDe = fixture.debugElement.query(By.css('li'));
+    liEl = liDe.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Deberia mostrar el texto test en el li tag', () => {
+    // li element deberia estar vacio
+    expect(liEl.textContent).toBe('');
+    // agregando el valor del Input() data
+    component.data = { id: 2, text: 'test' };
+    fixture.detectChanges();
+    expect(liEl.textContent).toBe('test')
+  });
+  
 });
